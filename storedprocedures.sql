8e766a05e8d348ce1654c229a70acf57f9e1364d
Anthony Marquez <spbomb@gmail.com>
2015-04-04 12:35:52 -0500

    Updated Syntax for T-SQL

--- storedprocedures.sql
+++ storedprocedures.sql
@@ -1,13 +1,15 @@
-DELIMITER //
+USE UnBenchMe
+GO
 
 CREATE PROCEDURE addRequest(
-rname varchar(255),
-rdesc text,
-rtime float(6,2),
-rmaxpeople tinyint,
-p_id int)
+@rname varchar(255),
+@rdesc text,
+@rtime float(6,2),
+@rmaxpeople tinyint,
+@p_id int)
+AS
 BEGIN
-	DECLARE r_id int;
+	DECLARE @r_id int;
 	-- Inserts new request
 	INSERT INTO Request(
 						RequestName,
@@ -15,23 +17,30 @@ BEGIN
                         RequestTime,
                         RequestMaxPeople)
 				Values(
-						rname,
-                        rdesc,
-                        rtime,
-                        rmaxpeople);
-	SET r_id = last_insert_id();
+						@rname,
+                        @rdesc,
+                        @rtime,
+                        @rmaxpeople);
+	SET @r_id = @@IDENTITY;
 	-- Associates user with request
 	INSERT INTO UserRequest(
 							Person_ID,
                             Request_ID)
 				Values(
-						p_id,
-                        r_id);
-END//
+						@p_id,
+                        @r_id);
+
+END
+
+/* Won't need this in v1 w/ skills (Anthony)
+
+USE UnBenchMe
+GO
 
 CREATE PROCEDURE addRequestSkill(
-r_id int,
+r_id int,	
 s_id int)
+AS
 BEGIN
 	INSERT INTO RequestSkill(
 							Request_ID,
@@ -40,37 +49,44 @@ BEGIN
 					Values(
 							r_id,
                             s_id);
-END//
+END
+
+*/
+GO
 
 CREATE PROCEDURE acceptTask(
-r_id int,
-p_id int)
+@r_id int,
+@p_id int)
+AS
 BEGIN
 	-- Adds person and request to TaskAccept
 	INSERT INTO TaskAccept(
 							Request_ID,
                             Person_ID)
 				Values(
-						r_id,
-                        p_id);
+						@r_id,
+                        @p_id);
 	
     -- Removes one person from max people needed, if 0 afterward, says it is filled
     UPDATE Request
 	SET
-		IsRequestFilled = CASE WHEN RequestMaxPeople < 2 THEN 1 END,
+		IsRequestFilled = CASE WHEN RequestMaxPeople <= 1 THEN 1 END,
 		RequestMaxPeople = CASE WHEN RequestMaxPeople > 0 THEN RequestMaxPeople - 1 END
 	WHERE
-		Request_ID = r_id;
+		Request_ID = @r_id;
         
-END//
+END
+
+GO
 
 CREATE PROCEDURE removePersonFromTask(
-r_id int,
-p_id int)
+@r_id int,
+@p_id int)
+AS
 BEGIN
 	-- Deletes person/request from task accept
 	DELETE FROM TaskAccept
-    WHERE Request_ID=r_id AND Person_ID=p_id;
+    WHERE Request_ID=@r_id AND Person_ID=@p_id;
 	
     -- Changes request to incomplete and adds one to max people needed
     UPDATE Request
@@ -78,16 +94,19 @@ BEGIN
 		IsRequestFilled = 0,
 		RequestMaxPeople = RequestMaxPeople + 1
 	WHERE
-		Request_ID = r_id;    
+		Request_ID = @r_id;    
 
-END//
+END
+
+GO
 
 CREATE PROCEDURE addPerson(
-uname varchar(255),
-fname varchar(255),
-lname varchar(255),
-email varchar(255)
+@uname varchar(255),
+@fname varchar(255),
+@lname varchar(255),
+@email varchar(255)
 )
+AS
 BEGIN
 	-- Adds person to person table
     INSERT INTO Person(
@@ -96,30 +115,37 @@ BEGIN
                         LastName,
 						EmailAddress)
 				Values(
-						uname,
-                        fname,
-                        lname,
-                        email);
-END//
+						@uname,
+                        @fname,
+                        @lname,
+                        @email);
+END
+
+GO
 
 CREATE PROCEDURE addCategory(
-cname varchar(255),
-cdesc text)
+@cname varchar(255),
+@cdesc text)
+AS
 BEGIN
 	-- Adds category to category table
     INSERT INTO Category(
 						CategoryName,
                         CategoryDesc)
 				Values(
-						cname,
-                        cdesc);
+						@cname,
+                        @cdesc);
 
-END//
+END
 
+/* Removed skill proc for v1 (Anthony)
+
+GO
 CREATE PROCEDURE addSkill(
-sname varchar(255),
-sdesc text,
-c_id int)
+@sname varchar(255),
+@sdesc text,
+@c_id int)
+AS
 BEGIN
 DECLARE s_id int;
 	-- Adds skill to skill table
@@ -140,3 +166,5 @@ DECLARE s_id int;
 								c_id,
                                 s_id);
 END//
+
+*/
