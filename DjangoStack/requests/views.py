0da05121eec2b425c6b86d3c98c371121af0755d
Anthony Marquez <anthonyraymarquez@gmail.com>
2015-04-06 07:49:51 -0500

    Created request view & template
    Created request view in request\views.py and template in
    \requests\templates\requests\index.html

--- DjangoStack/requests/views.py
+++ DjangoStack/requests/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from .models import Category
+from .models import Request
+from .models import RequestAccept
+from .models import Available
+
+# views go here
+
+def activeRequests(request):
+    #all active requests in list format, boring i know
+    active_request_list = Request.objects
+    output = ', '.join([p.username for p in active_request_list])               # Anthony, reference by PK not username
+    return HttpResponse(output)
+
+def 
